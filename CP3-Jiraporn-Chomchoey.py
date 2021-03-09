a417580d05186a89882e69a8cd247b302498bb68
Barbossa230260 <fmvno2515@gmail.com>
2021-03-09 15:49:21 +0700

    CP3-Jiraporn-Chomchoey.py

---
+++ CP3-Jiraporn-Chomchoey.py
@@ -0,0 +1,10 @@
+FD_ENG = 55.5
+GL_BS = 90
+ICS = 75
+COMP = 82.5
+
+print("-----Your Score-----")
+print("Foundation English :",FD_ENG)
+print("General Business :",GL_BS)
+print("Introduction to Computer Systems  :",ICS)
+print("Computer Programming :",COMP)
